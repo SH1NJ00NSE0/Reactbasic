099a919e1bb79cfdac39c0ae7e5176dfd14543b5
baeian <ddoory1103@gmail.com>
2022-07-25 16:51:18 +0900

    버튼 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
    return (
       <div className="App">
+         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
          <div className='header'>
             <div className='logo'>
                <a href='#'>
@@ -25,8 +26,23 @@ function App() {
             <img className='bg' src='img/section_bgimg.svg'></img>
             <h1 className='section_title'>React</h1>
             <p className='section_description'>A JavaScript library for building user interfaces</p>
+            <div className='button_wrap'>
+               <div className='get_started'>Get Started</div>
+               <div className='tutorial'>
+                  <span>Take the Tutorial</span>
+                  <span class="material-symbols-rounded">arrow_forward_ios</span>
+               </div>
+            </div>
          </div>
          <div className='main'>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
+            <h1>sfsfd</h1>
             <h1>sfsfd</h1>
             <h1>sfsfd</h1>
             <h1>sfsfd</h1>
