85dcbcb6a53d1ff7610a980ae88768ca6bf25cbf
baeian <ddoory1103@gmail.com>
2022-07-25 14:33:12 +0900

    리액트 홈페이지 클론코딩

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+   let titleName = 'React';
+
+   return (
+      <div className="App">
+         <div className='header'>
+            <div className='logo'>
+               <a href='#'>
+                  <img src='img/React.svg'></img>
+                  <span className='title'>{titleName}</span>
+               </a>
+            </div>
+            <ul className='nav'>
+               <li><a href='#'>Docs</a></li>
+               <li><a href='#'>Tutorial</a></li>
+               <li><a href='#'>Blog</a></li>
+               <li><a href='#'>Community</a></li>
+            </ul>
+         </div>
+         <div className='section'>
+            <img className='bg' src='img/section_bgimg.svg'></img>
+            <h1 className='section_title'>React</h1>
+            <p className='section_description'>사용자 인터페이스를 만들기 위한 JavaScript 라이브러리</p>
+         </div>
+      </div>
+   );
 }
 
 export default App;
